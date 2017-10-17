7a52364eec2950b880d587490bb843fc1cfd0979
vbagustinus <vbagustinus@gmail.com>
2017-10-17 11:26:28 +0700

    broken palindrome

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -7,20 +7,26 @@
  */
 function palindromeSwapper(str) {
   let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+  for (var i = 0; i <= str.length; i++) {
+     newStr = '';
+    for (var j = 0; j < str.length;j++) {
       if (i === j) {
-        newStr += str[j++];
+        // debugger
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
     }
+    // console.log('~~~~~~~~~~~~~~',newStr);
+    // console.log('--------------',str);
+
     if (isPalindrome(newStr)) return true;
   }
   return false;
 }
+
 
 /**
  * @function isPalindrome
@@ -29,8 +35,10 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  // console.log('===============',str);
+  // console.log('---------------',str.split(' ').reverse().join(''));
+  if (str.split('').reverse().join('') === str) return true;
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
